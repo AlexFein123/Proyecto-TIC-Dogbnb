f30ce0dbeb5b432b48562452b557983f6e0954a4
Nacho <ifranco@correo.um.edu.uy>
2025-09-24 11:18:56 -0300

    Borrando la carpeta DogBNB, ya tiene su repo propio

--- dogbnb-app/src/background.tsx
+++
@@ -1,9 +0,0 @@
-import "./background.css";
-
-export default function background(){
-    return(
-        <div className="image-container">
-            <img className="background-image" src="/public/background-dog.jpg" alt="Imagen de fondo con perros" />
-        </div>
-    )
-}
